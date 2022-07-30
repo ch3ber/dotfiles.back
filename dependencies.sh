6141c0465b31c7ba14ad5fe571b3f06b2a495c07
Eber Alejo <eber4202@hotmail.com>
2022-07-29 19:05:59 -0500

    [FEAT] dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 echo Installing dependencies ...
-sudo pacman -S bspwm sxhkd neovim alacritty
-
-# plugged
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
+sudo pacman -S alacritty bspwm neovim picom polybar sxhkd
 echo Finish.
